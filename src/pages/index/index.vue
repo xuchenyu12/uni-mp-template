4c59497276dde12495b6e9bc6e528aee1631bfad
徐晨瑜 <11094724+xuchen-12@user.noreply.gitee.com>
2023-11-08 16:04:14 +0800

    20231108

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,6 +5,10 @@
 <template>
   <view class="index">index</view>
   <div class="ellipsis">哪里贵了? 这么多年都是这个价格</div>
+  <div class="flex justify-center h-100 items-center">
+    <div class="w-50 h-50 bg-pink-500"></div>
+  </div>
+  <div class="test h-100 w-730 bg-yellow-200"></div>
 </template>
 
 <style lang="scss">
